128d9033f773fd3425dfb49db971cf4cd5cc3348
Shathees1234 <shatheesg833@gmail.com>
2025-12-19 13:00:04 +0530

    Create ecommerce_db and products table

---
+++ backend/db.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE IF NOT EXISTS ecommerce_db;
+USE ecommerce_db;
+
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL
+);
